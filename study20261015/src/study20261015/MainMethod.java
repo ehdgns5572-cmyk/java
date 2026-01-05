278f6ca0df76ace2d9b2db3579832d6605954eff
DW-203 <DW-203@DESKTOP-71GUKGJ>
2026-01-05 10:53:23 +0900

    1월 5일 if문 주차요금

---
+++ study20261015/src/study20261015/MainMethod.java
@@ -0,0 +1,15 @@
+package study20261015;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+		// Integer.parseInt ()   정수값으로 변경할 수 있다.
+		// Math.floor()     소수점 자르기
+  		int num = (int)Math.floor(Math.random()*10);
+		
+		System.out.println( num );
+
+	}
+
+}
